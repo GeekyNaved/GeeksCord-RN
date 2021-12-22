fbd87698337f5ba345d42e475217133bba06c91d
GeekyNaved <navedahmed040@gmail.com>
2021-12-22 15:53:01 +0530

    uploading code in git

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar />
+      <Text>hello naved</Text>
+    </>
+  );
+};
+
+export default App;
